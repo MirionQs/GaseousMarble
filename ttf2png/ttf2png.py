b468fdecae61e9310410e455523bd189983b84af
Mirion <51433180+MirionQs@users.noreply.github.com>
2024-07-01 23:55:09 +0800

    only convert characters with Unicode <= 65535

--- ttf2png/ttf2png.py
+++ ttf2png/ttf2png.py
@@ -4,20 +4,20 @@ from PIL import Image, ImageFont, ImageDraw
 
 def ttf2png(font_path: str, font_size_pt: int, png_path: str, gly_path: str, char_list: str | None = None, max_width: int = 0x3fff):
     ttfont = TTFont(font_path)
-    char_code = []
+    code_list = set()
     for table in ttfont['cmap'].tables:
-        char_code += table.cmap.keys()
+        code_list |= table.cmap.keys()
     if char_list != None:
-        char_code += list(map(ord, char_list))
-    char_code = list(set(char_code))
-    char_code.sort()
+        code_list &= map(ord, char_list)
+    code_list = list(filter(lambda ch: ch <= 0xffff, code_list))
+    code_list.sort()
 
     font = ImageFont.truetype(font_path, font_size_pt * 4 / 3)
     line_width = 0
     line_count = 1
     line_height = 0
-    for ch in char_code:
-        left, top, right, bottom = font.getbbox(chr(ch))
+    for c in code_list:
+        left, top, right, bottom = font.getbbox(chr(c))
         width = right - left
         if line_width + width >= max_width:
             line_width = 0
@@ -32,14 +32,14 @@ def ttf2png(font_path: str, font_size_pt: int, png_path: str, gly_path: str, cha
         gly.write(b'GLY\0' + font_size_pt.to_bytes() + line_height.to_bytes())
         x = 0
         y = 0
-        for ch in char_code:
-            left, top, right, bottom = font.getbbox(chr(ch))
+        for c in code_list:
+            left, top, right, bottom = font.getbbox(chr(c))
             width = right - left
             if x + width >= max_width:
                 x = 0
                 y += line_height
-            draw.text((x - left, y), chr(ch), 'white')
-            gly.write(ch.to_bytes(2) + x.to_bytes(2) + y.to_bytes(2) + width.to_bytes() + left.to_bytes(signed=True))
+            draw.text((x - left, y), chr(c), 'white')
+            gly.write(c.to_bytes(2) + x.to_bytes(2) + y.to_bytes(2) + width.to_bytes() + left.to_bytes(signed=True))
             x += width + 1
     image.save(png_path)
 
