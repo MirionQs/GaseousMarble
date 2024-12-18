2cc9aa6e5371b188f0ec4f904f6c1cb99dccbbce
Mirion <51433180+MirionQs@users.noreply.github.com>
2024-12-18 15:25:18 +0800

    improve: filter control characters except '\n'

--- utils.h
+++ utils.h
@@ -8,17 +8,29 @@ namespace gm {
         std::wstring ret;
         auto i{ str.begin() }, end{ str.end() };
         while (i != end) {
+            wchar_t ch;
             if ((*i & 0x80) == 0) {
-                ret += i[0];
+                ch = i[0];
                 i += 1;
             }
             else if ((*i & 0xe0) == 0xc0) {
-                ret += (i[0] & 0x1f) << 6 | i[1] & 0x3f;
+                ch = (i[0] & 0x1f) << 6 | i[1] & 0x3f;
                 i += 2;
             }
             else if ((*i & 0xf0) == 0xe0) {
-                ret += i[0] << 12 | (i[1] & 0x3f) << 6 | i[2] & 0x3f;
+                ch = i[0] << 12 | (i[1] & 0x3f) << 6 | i[2] & 0x3f;
                 i += 3;
+            }
+            else {
+                ++i;
+                continue;
+            }
+
+            if (iswblank(ch)) {
+                ret.push_back(' ');
+            }
+            else if (!iswcntrl(ch) || ch == '\n') {
+                ret.push_back(ch);
             }
         }
         return ret;
