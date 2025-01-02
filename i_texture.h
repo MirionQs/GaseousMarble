4bf399746c183d3d34b13e84bf3d989e2ac16eea
Mirion <51433180+MirionQs@users.noreply.github.com>
2025-01-02 15:21:02 +0800

    add texture interface

---
+++ i_texture.h
@@ -0,0 +1,28 @@
+#pragma once
+
+namespace gm {
+
+    namespace api {
+
+        class ITexture {
+            struct TextureData {
+                void* texture; // IDirect3DTexture8
+                std::size_t image_width, image_height;
+                std::size_t texture_width, texture_height;
+                bool is_valid;
+            };
+
+            TextureData** _textures;
+            std::size_t* _size;
+
+        public:
+            ITexture() noexcept :
+                _textures{ reinterpret_cast<TextureData**>(0x0085b3c4) },
+                _size{ reinterpret_cast<std::size_t*>(0x006886f0) } {};
+        };
+
+        inline gm::api::ITexture texture;
+
+    }
+
+}
