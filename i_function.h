3a0c06a828662e1d3ad85e1f72f92cb486da835e
Mirion <51433180+MirionQs@users.noreply.github.com>
2025-01-02 11:10:01 +0800

    make call private

--- i_function.h
+++ i_function.h
@@ -30,12 +30,8 @@ namespace gm {
 
             FunctionResource* _resource;
 
-        public:
-            IFunction() noexcept :
-                _resource{ reinterpret_cast<FunctionResource*>(0x00686b1c) } {};
-
             template<class R, class... Args>
-            R call(FunctionId id, Args... args) const noexcept {
+            R _call(FunctionId id, Args... args) const noexcept {
                 gm::api::Value args_wrapped[]{ args... }, ret;
 
                 gm::api::Value* argv{ args_wrapped };
@@ -52,16 +48,20 @@ namespace gm {
                 return static_cast<R>(ret);
             }
 
-            void draw_sprite_general(std::size_t sprite_id, std::size_t image_index, double x, double y, std::size_t width, std::size_t height, double offset_x, double offset_y, double scale_x, double scale_y, double rotate, std::uint32_t color1, std::uint32_t color2, std::uint32_t color3, std::uint32_t color4, double alpha) const noexcept {
-                call<void, gm::api::Real, gm::api::Real, gm::api::Real, gm::api::Real, gm::api::Real, gm::api::Real, gm::api::Real, gm::api::Real, gm::api::Real, gm::api::Real, gm::api::Real, gm::api::Real, gm::api::Real, gm::api::Real, gm::api::Real, gm::api::Real>(FunctionId::draw_sprite_general, sprite_id, image_index, x, y, width, height, offset_x, offset_y, scale_x, scale_y, rotate, color1, color2, color3, color4, alpha);
+        public:
+            IFunction() noexcept :
+                _resource{ reinterpret_cast<FunctionResource*>(0x00686b1c) } {};
+
+            void draw_sprite_general(std::size_t sprite_id, std::int32_t image_index, std::uint32_t x, std::uint32_t y, std::size_t width, std::size_t height, double offset_x, double offset_y, double scale_x, double scale_y, double rotate, std::uint32_t color1, std::uint32_t color2, std::uint32_t color3, std::uint32_t color4, double alpha) const noexcept {
+                _call<void, gm::api::Real, gm::api::Real, gm::api::Real, gm::api::Real, gm::api::Real, gm::api::Real, gm::api::Real, gm::api::Real, gm::api::Real, gm::api::Real, gm::api::Real, gm::api::Real, gm::api::Real, gm::api::Real, gm::api::Real, gm::api::Real>(FunctionId::draw_sprite_general, sprite_id, image_index, x, y, width, height, offset_x, offset_y, scale_x, scale_y, rotate, color1, color2, color3, color4, alpha);
             }
 
             std::size_t sprite_add(std::string_view filename, std::size_t image_number, bool remove_background, bool smooth, std::int32_t x, std::int32_t y) const noexcept {
-                return call<std::size_t, gm::api::String, gm::api::Real, gm::api::Real, gm::api::Real, gm::api::Real, gm::api::Real>(FunctionId::sprite_add, filename.data(), image_number, remove_background, smooth, x, y);
+                return _call<std::size_t, gm::api::String, gm::api::Real, gm::api::Real, gm::api::Real, gm::api::Real, gm::api::Real>(FunctionId::sprite_add, filename.data(), image_number, remove_background, smooth, x, y);
             }
 
             void sprite_delete(std::size_t sprite_id) const noexcept {
-                call<void, gm::api::Real>(FunctionId::sprite_delete, sprite_id);
+                _call<void, gm::api::Real>(FunctionId::sprite_delete, sprite_id);
             }
         };
 
