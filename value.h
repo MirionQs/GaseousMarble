1fb86e92c214633d1525de4df48b63581b9346ae
Mirion <51433180+MirionQs@users.noreply.github.com>
2025-01-29 14:33:55 +0800

    improve value.h

--- value.h
+++ value.h
@@ -9,25 +9,24 @@ namespace gm {
 
     namespace api {
 
+        enum class ValueType {
+            real,
+            string
+        };
+
         class Value {
-            bool _is_string;
-            gm::api::Real _real;
-            gm::api::String _string;
+            ValueType _type{ ValueType::real };
+            gm::api::Real _real{ 0. };
+            gm::api::String _string{ nullptr };
 
         public:
-            Value() noexcept :
-                _is_string{},
-                _real{},
-                _string{} {}
+            Value() noexcept = default;
 
             Value(gm::api::Real num) noexcept :
-                _is_string{},
-                _real{ num },
-                _string{} {}
+                _real{ num } {}
 
             Value(gm::api::String str) noexcept :
-                _is_string{ true },
-                _real{} {
+                _type{ ValueType::string } {
 
                 assert(str != nullptr);
 
@@ -40,8 +39,7 @@ namespace gm {
                 _string = pascal_str + 12;
             }
 
-            Value(const Value& other) noexcept :
-                _is_string{} {
+            Value(const Value& other) noexcept {
                 *this = other;
             };
 
@@ -50,46 +48,50 @@ namespace gm {
             };
 
             ~Value() noexcept {
-                if (_is_string) {
+                if (_type == ValueType::string) {
                     delete[](_string - 12);
                 }
             }
 
             Value& operator=(const Value& other) noexcept {
-                if (_is_string) {
+                if (_type == ValueType::string) {
                     delete[](_string - 12);
                 }
 
-                _is_string = other._is_string;
-                if (_is_string) {
+                _type = other._type;
+                if (_type == ValueType::real) {
+                    _real = other._real;
+                }
+                else {
                     std::size_t len{ *reinterpret_cast<const std::size_t*>(other._string - 4) };
                     char* str{ new char[len + 1] };
                     std::memcpy(str, other._string - 12, len + 1);
 
                     _string = str + 12;
                 }
-                else {
-                    _real = other._real;
-                }
 
                 return *this;
             }
 
             Value& operator=(Value&& other) noexcept {
-                _is_string = other._is_string;
+                _type = other._type;
                 _real = other._real;
                 _string = std::exchange(other._string, nullptr);
                 return *this;
             }
 
             operator gm::api::Real() const noexcept {
-                assert(!_is_string);
+                assert(_type == ValueType::real);
                 return _real;
             }
 
             operator gm::api::String() const noexcept {
-                assert(_is_string);
+                assert(_type == ValueType::string);
                 return _string;
+            }
+
+            ValueType type() const noexcept {
+                return _type;
             }
         };
 
