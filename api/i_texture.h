7d2d00bb884b5e40a1ef5fd6dd53f54efe523466
Mirion <51433180+MirionQs@users.noreply.github.com>
2025-02-06 00:17:16 +0800

    add texture.h

--- api/i_texture.h
+++ api/i_texture.h
@@ -1,32 +1,23 @@
 #pragma once
 
-#include "../utils/int.h"
-
-#include <d3dx8.h>
+#include "texture.h"
 
 namespace gm {
 
     namespace api {
 
         class ITexture {
-            struct TextureData {
-                IDirect3DTexture8* texture;
-                gm::u32 image_width, image_height;
-                gm::u32 texture_width, texture_height;
-                bool is_valid;
-            };
-
-            TextureData** _textures;
+            Texture** _textures;
             gm::u32* _count;
 
         public:
             ITexture() noexcept :
-                _textures{ reinterpret_cast<TextureData**>(0x0085b3c4) },
+                _textures{ reinterpret_cast<Texture**>(0x0085b3c4) },
                 _count{ reinterpret_cast<gm::u32*>(0x006886f0) } {};
 
-            //const Texture& operator[](gm::u32 id) const noexcept {
-            //    return (*_textures)[id];
-            //}
+            const Texture& operator[](gm::u32 id) const noexcept {
+                return (*_textures)[id];
+            }
 
             gm::u32 count() const noexcept {
                 return *_count;
---
+++ api/texture.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "../utils/int.h"
+
+#include <d3dx8.h>
+
+namespace gm {
+
+    namespace api {
+
+        class Texture {
+            struct Size {
+                gm::u32 width, height;
+            };
+
+            IDirect3DTexture8* _data;
+            Size _image_size;
+            Size _texture_size;
+            bool _is_valid;
+
+        public:
+            Texture() = delete;
+            Texture(const Texture&) = delete;
+            Texture(Texture&&) = delete;
+
+            Texture& operator=(const Texture&) = delete;
+            Texture& operator=(Texture&&) = delete;
+
+            decltype(auto) image_size(this auto& self) noexcept {
+                return self._image_size;
+            }
+
+            decltype(auto) texture_size(this auto& self) noexcept {
+                return self._texture_size;
+            }
+
+            decltype(auto) data(this auto& self) noexcept {
+                return self._data;
+            }
+        };
+
+    }
+
+}
