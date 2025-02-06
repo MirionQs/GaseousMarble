b15f632da43856bbda1582e91157f1bad1ba43c4
Mirion <51433180+MirionQs@users.noreply.github.com>
2025-02-06 18:15:15 +0800

    add assertions and set_texture for Sprite

--- api/sprite.h
+++ api/sprite.h
@@ -46,11 +46,18 @@ namespace gm {
             }
 
             auto&& bitmap(this auto&& self, gm::u32 index) noexcept {
+                assert(index < self._data->subimage_count);
                 return std::forward_like<decltype(self)>(*self._data->bitmaps[index]);
             }
 
             auto&& texture(this auto&& self, gm::u32 index) noexcept {
+                assert(index < self._data->subimage_count);
                 return std::forward_like<decltype(self)>(gm::api::texture[self._data->texture_ids[index]]);
+            }
+
+            void set_texture(gm::u32 index, gm::u32 id) noexcept {
+                assert(index < _data->subimage_count && id < gm::api::texture.count());
+                _data->texture_ids[index] = id;
             }
         };
 
